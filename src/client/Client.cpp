8dff2e551f99ad0bad3e3663c2fa549435f9d85f
Jene Litsch <litsch.jene@gmail.com>
2022-11-19 18:07:38 +0100

    Fixed aspect ration

--- src/client/Client.cpp
+++ src/client/Client.cpp
@@ -4,8 +4,36 @@
 #include <iostream>
 
 namespace client {
+	namespace {
+		sf::FloatRect calc_view_port(
+			const sf::Window & window,
+			const sf::View & view){
+
+			const float aspect_ratio_window
+					= float(window.getSize().x)
+					/ float(window.getSize().y);
+
+			const float aspect_ratio_buffer
+					= float(view.getSize().x)
+					/ float(view.getSize().y);
+
+			if(aspect_ratio_buffer > aspect_ratio_window){
+					const float relative_height = aspect_ratio_window/aspect_ratio_buffer;
+					const float offset = (1.f - relative_height) / 2.f;
+					return sf::FloatRect(0.f, offset, 1.f, relative_height);
+			}
+			else{
+					const float relative_width = aspect_ratio_buffer/aspect_ratio_window;
+					const float offset = (1.f - relative_width) / 2.f;
+					return sf::FloatRect(offset, 0.f, relative_width, 1.f);
+			}	
+		}
+	}
+
 	Client::Client() {
+		sf::View view{{128,128}, {256,256}};
 		this->window.create(sf::VideoMode{256,256}, "Creature Simulator");
+		this->window.setView(view);
 		this->state_manager.push(std::make_unique<session::HostSession>());
 	}
 
@@ -42,6 +70,8 @@ namespace client {
 
 
 	void Client::on_event(const core::WindowResized & event) {
-		// this->
+		auto view = this->window.getView();
+		view.setViewport(calc_view_port(this->window, view));
+		this->window.setView(view);
 	}
 }
