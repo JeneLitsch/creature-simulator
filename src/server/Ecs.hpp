8b3033ea3c00a4ca74d14e2926676388bb87e6c1
Jonas Regh <Jonas Regh>
2023-05-31 16:35:03 +0200

    Added Movement and Age Components

--- src/server/Ecs.hpp
+++ src/server/Ecs.hpp
@@ -3,7 +3,9 @@
 #include "server/component/PheromoneEmitter.hpp"
 #include "server/component/Sensors.hpp"
 #include "server/component/Metabolism.hpp"
+#include "server/component/Movement.hpp"
+#include "server/component/Age.hpp"
 
 namespace server {
-	using Ecs =	nc::Ecs<PheromoneEmitter, Sensors, Metabolism>;
+	using Ecs =	nc::Ecs<PheromoneEmitter, Sensors, Metabolism, Movement, Age>;
 }
--- src/server/component/Movement.hpp
+++ src/server/component/Movement.hpp
@@ -1,8 +1,24 @@
 #pragma once
 #include "stdxx/vector.hxx"
+#include "stdxx/math.hxx"
+#include "stdxx/grid.hxx"
 
 namespace server {
 	struct Movement {
+		stx::vector2i position;
 		stx::vector2i direction;
+		stx::grid2<std::uint64_t>* grid;
+
+		void move(){
+			direction.x = rand() % 3 - 1;
+			direction.y = rand() % 3 - 1;
+			std::uint64_t& id = (*grid)[position];
+			stx::vector2i new_position = stx::clamp(position + direction, stx::vector2i{0,0}, stx::vector2i {grid->size().x-1, grid->size().y-1});
+			std::uint64_t& other_id = (*grid)[new_position];
+			if(other_id < 256){
+				position = new_position;
+				std::swap(id, other_id);
+			}
+		}
 	};
 }
