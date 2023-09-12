b15ddb6f52bdee2727ed6a6ac04b87b833dcdbf3
Jonas Regh <Jonas Regh>
2023-09-12 16:53:38 +0200

    added more color variety (hopefully)

--- src/sim/component/Sprite.hpp
+++ src/sim/component/Sprite.hpp
@@ -14,15 +14,31 @@ namespace sim {
 			int difference = interval(rng);
             difference = difference - mutationDampener * difference;
 			int new_value = child.color.r + difference;
-			child.color.r = std::clamp(new_value, 128, UINT8_MAX);
+			child.color.r = std::clamp(new_value, 0, UINT8_MAX);
 			difference = interval(rng);
             difference = difference - mutationDampener * difference;
 			new_value = child.color.g + difference;
-			child.color.g = std::clamp(new_value, 128, UINT8_MAX);
+			child.color.g = std::clamp(new_value, 0, UINT8_MAX);
 			difference = interval(rng);
             difference = difference - mutationDampener * difference;
 			new_value = child.color.b + difference;
-			child.color.b = std::clamp(new_value, 128, UINT8_MAX);
+			child.color.b = std::clamp(new_value, 0, UINT8_MAX);
+			int sum = child.color.r + child.color.g + child.color.b;
+			while(sum < 350){
+				int index = rng() % 3;
+				switch(index){
+					case 0:
+						child.color.r++;
+						break;
+					case 1:
+						child.color.g++;
+						break;
+					case 2:
+						child.color.b++;
+						break;
+				}
+				sum++;
+			}
             return child;
 		}
 	};
